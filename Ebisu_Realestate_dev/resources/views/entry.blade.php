8b890857f0ccc49e454f7b1a4d4930b2bd8fccee
yo-nakano <ynakano7621@gmail.com>
2022-03-26 20:48:32 +0900

    エントリーページ作業中

--- Ebisu_Realestate_dev/resources/views/entry.blade.php
+++ Ebisu_Realestate_dev/resources/views/entry.blade.php
@@ -1,8 +1,11 @@
 @extends('layout2')
 
 @section('content')
+<?php
+  $prefectures = array('北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県');
+?>
 
-<div class="container-fluid" style="padding: 100px 150px 0;">
+<div class="container" style="padding: 100px 150px 0;">
   <h1 class="p-title gold-title center-block text-center">PROPERTY ENTRY</h1>
   <p class="p-title center-block text-center h3 mb-5">物件エントリー</p>
   <div class="main-content row px-5">
@@ -82,6 +85,9 @@
             <div>
               <select>
                 <option value="">選択</option>
+                @foreach ($prefectures as $prefecture)
+                <option value="{{ $prefecture }}">{{$prefecture}}</option>
+                @endforeach
               </select>
             </div>
             <div>
@@ -109,6 +115,17 @@
           <td>
             <select>
               <option value="">選択</option>
+              <option value="">会社員</option>
+              <option value="">会社役員</option>
+              <option value="">経営者</option>
+              <option value="">公務員</option>
+              <option value="">商工自営（個人事業）</option>
+              <option value="">医師</option>
+              <option value="">教職員</option>
+              <option value="">弁護士・会計士</option>
+              <option value="">団体職員</option>
+              <option value="">無職</option>
+              <option value="">その他</option>
             </select>
           </td>
         </tr>
@@ -118,6 +135,15 @@
           <td>
             <select>
               <option value="">選択</option>
+              <option value="">300万円未満</option>
+              <option value="">300万〜400万円</option>
+              <option value="">401万〜500万円</option>
+              <option value="">501万〜600万円</option>
+              <option value="">601万〜700万円</option>
+              <option value="">701万〜800万円</option>
+              <option value="">801万〜900万円</option>
+              <option value="">901万〜1000万円</option>
+              <option value="">1000万円以上</option>
             </select>
           </td>
         </tr>
@@ -127,6 +153,13 @@
           <td>
             <select>
               <option value="">選択</option>
+              <option value="">500万円未満</option>
+              <option value="">501万〜600万円</option>
+              <option value="">601万〜700万円</option>
+              <option value="">701万〜800万円</option>
+              <option value="">801万〜900万円</option>
+              <option value="">901万〜1000万円</option>
+              <option value="">1000万円以上</option>
             </select>
           </td>
         </tr>
@@ -136,6 +169,16 @@
           <td>
             <select>
               <option value="">選択</option>
+              <option value="">家賃の無駄</option>
+              <option value="">狭い</option>
+              <option value="">古い</option>
+              <option value="">環境が悪い</option>
+              <option value="">不便（通勤・通学・買い物）</option>
+              <option value="">転勤のため</option>
+              <option value="">結婚・世帯分離</option>
+              <option value="">老後のため</option>
+              <option value="">セカンドハウス</option>
+              <option value="">その他</option>
             </select>
           </td>
         </tr>
@@ -145,6 +188,14 @@
           <td>
             <select>
               <option value="">選択</option>
+              <option value="">マンション（賃貸）</option>
+              <option value="">マンション（分譲賃貸）</option>
+              <option value="">マンション（持家）</option>
+              <option value="">戸建（持家）</option>
+              <option value="">戸建（借家）</option>
+              <option value="">社宅・官舎</option>
+              <option value="">実家</option>
+              <option value="">その他</option>
             </select>
           </td>
         </tr>
@@ -159,6 +210,15 @@
           <td>
             <select>
               <option value="">選択</option>
+              <option value="">〜50m&sup2;</option>
+              <option value="">〜55m&sup2;</option>
+              <option value="">〜60m&sup2;</option>
+              <option value="">〜65m&sup2;</option>
+              <option value="">〜70m&sup2;</option>
+              <option value="">〜75m&sup2;</option>
+              <option value="">〜80m&sup2;</option>
+              <option value="">〜85m&sup2;</option>
+              <option value="">90m&sup2;以上</option>
             </select>
           </td>
         </tr>
