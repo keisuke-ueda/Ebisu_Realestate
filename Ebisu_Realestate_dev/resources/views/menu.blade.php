c321b3714f0c4f19860f648c63cc1e590c951393
yo-nakano <ynakano7621@gmail.com>
2022-04-05 21:09:38 +0900

    余白修正中

--- Ebisu_Realestate_dev/resources/views/menu.blade.php
+++ Ebisu_Realestate_dev/resources/views/menu.blade.php
@@ -1,13 +1,11 @@
 <!-- ロゴ -->
-<div class="index_logo position-absolute mt-3 ps-3 logo_animation col-2" style="z-index: 1000;">
+<div class="index_logo position-absolute mt-3 ps-4 logo_animation col-2" style="z-index: 1000;">
   <a href="/">
-    <div class="d-flex flex-column align-items-center">
-      <img src="{{ asset('/images/ebisulogo.png') }}" alt="ヘッダーロゴ" class="mb-2" style="width: 120px;">
-    </div>
+    <img src="{{ asset('/images/ebisulogo.png') }}" alt="ヘッダーロゴ" class="" style="width: 120px;">
   </a>
 </div>
 
 <!-- ハンバーガーボタン -->
-<button class="menu-trigger menu_animation position-absolute top-0 end-0 mt-3 me-3">
+<button class="menu-trigger menu_animation position-absolute top-0 end-0 mt-3 me-4">
   <img class="w-100" src="{{ asset('/images/menu_btn.png') }}" alt="メニューボタン">
 </button>
