d425a562c33872969391da42f59a3b36076f0753
yo-nakano <ynakano7621@gmail.com>
2022-04-06 15:45:45 +0900

    自動改行阻止

--- Ebisu_Realestate_dev/resources/views/header.blade.php
+++ Ebisu_Realestate_dev/resources/views/header.blade.php
@@ -31,15 +31,15 @@
       <div class="header_link ms-3">
         <div>&nbsp;</div>
         <div class="d-flex align-items-auto"><p class="s10 ft1 blank">&nbsp;</p></div>
-        <a href="/model_room" style="pointer-events:none;" class="small mh-parts gray_link">MODEL ROOM</a>
+        <a href="/model_room" style="pointer-events:none; white-space:nowrap;" class="small mh-parts gray_link">MODEL ROOM</a>
       </div>
       <div class="header_link ms-3">
-        <div><a href="/overview" class="s10 mh-parts white_link">>物件概要</a></div>
+        <div><a href="/overview" class="s10 mh-parts white_link" style="white-space:nowrap;">>物件概要</a></div>
         <div class="d-flex align-items-auto"><p class="s10 ft1 blank">&nbsp;</p></div>
         <a href="/plan" style="pointer-events:none;" class="small mh-parts gray_link">PLAN</a>
       </div>
       <div class="header_link ms-3">
-        <div><a href="/map" class="s10 mh-parts white_link">>現地案内図</a></div>
+        <div><a href="/map" class="s10 mh-parts white_link" style="white-space:nowrap;">>現地案内図</a></div>
         <div class="d-flex align-items-auto"><p class="s10 ft1 blank">&nbsp;</p></div>
         <a href="/blanding" style="pointer-events:none;" class="small mh-parts gray_link">BLAND</a>
       </div>
