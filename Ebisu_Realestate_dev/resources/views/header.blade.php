7497dddcceda03ebefbe8d6ee565fb73fae50102
keisuke-ueda <81659758+keisuke-ueda@users.noreply.github.com>
2022-04-07 00:51:56 +0900

    ヘッダー修正完了

--- Ebisu_Realestate_dev/resources/views/header.blade.php
+++ Ebisu_Realestate_dev/resources/views/header.blade.php
@@ -1,31 +1,35 @@
-<header class="header text-white position-fixed top-0 start-0 px-4 py-3 d-none">
+<header class="header text-white position-fixed top-0 start-0 d-none">
   <div class="header_content">
+
     <div class="header_title">
       <a href="/" class="">
-        <img src="{{ asset('/images/ebisulogo.png') }}" alt="ヘッダーロゴ" class="mh-parts" style="width: 120px;">
+        <img src="{{ asset('/images/ebisulogo.png') }}" alt="ヘッダーロゴ" class="mh-parts logo_s">
       </a>
     </div>
+
     <div class="header_links">
-      
       <div class="header_link ms-3">
         <div>&nbsp;</div>
         <div class="d-flex align-items-auto"><p class="s10 ft1 new-mark">NEW</p></div>
         <a href="/concept" class="small mh-parts white_link">EBISU</a>
       </div>
+
       <div class="header_link ms-3">
         <div>&nbsp;</div>
-        <div class="d-flex align-items-auto"><p class="s10 ft1 new-mark">NEW</p></div>
-        <a href="/location" style="pointer-events:none;" class="small mh-parts white_link">LOCATION</a>
+        <!-- <div class="d-flex align-items-auto"><p class="s10 ft1 new-mark">NEW</p></div> -->
+        <a href="/location" style="pointer-events:none; margin-top:13px;" class="small mh-parts gray_link">LOCATION</a>
       </div>
+
       <div class="header_link ms-3">
         <div>&nbsp;</div>
         <div class="d-flex align-items-auto"><p class="s10 ft1 new-mark">NEW</p></div>
         <a href="/access" class="small mh-parts white_link">ACCESS</a>
       </div>
+
       <div class="header_link ms-3">
         <div>&nbsp;</div>
-        <div class="d-flex align-items-auto"><p class="s10 ft1 new-mark">NEW</p></div>
-        <a href="/design" style="pointer-events:none;" class="small mh-parts white_link">DESIGN</a>
+        <!-- <div class="d-flex align-items-auto"><p class="s10 ft1 new-mark">NEW</p></div> -->
+        <a href="/design" style="pointer-events:none; margin-top:13px;" class="small mh-parts gray_link">DESIGN</a>
       </div>
 
       <div class="header_link ms-3">
@@ -33,11 +37,13 @@
         <div class="d-flex align-items-auto"><p class="s10 ft1 blank">&nbsp;</p></div>
         <a href="/model_room" style="pointer-events:none; white-space:nowrap;" class="small mh-parts gray_link">MODEL ROOM</a>
       </div>
+
       <div class="header_link ms-3">
         <div><a href="/overview" class="s10 mh-parts white_link" style="white-space:nowrap;">>物件概要</a></div>
         <div class="d-flex align-items-auto"><p class="s10 ft1 blank">&nbsp;</p></div>
         <a href="/plan" style="pointer-events:none;" class="small mh-parts gray_link">PLAN</a>
       </div>
+
       <div class="header_link ms-3">
         <div><a href="/map" class="s10 mh-parts white_link" style="white-space:nowrap;">>現地案内図</a></div>
         <div class="d-flex align-items-auto"><p class="s10 ft1 blank">&nbsp;</p></div>
@@ -46,17 +52,20 @@
 
       <div class="header_link ms-3">
         <a href="/entry" class="mh-parts">
-          <button type="button" class="s13 entry_btn text-white" style="height: 4.17vw; width: 15vw;">物件エントリー</button>
+          <button type="button" class="s13 entry_btn text-white" style="height: 40px; width: 135px;">物件エントリー</button>
         </a>
       </div>
+
       <div class="header_link ms-3">
         <a href="/questionnaire">
-          <button type="button" class="s13 questionnaire_btn text-white mh-parts" style="height: 4.17vw; width: 15vw;">お住まいのアンケート</button>
+          <button type="button" class="s13 questionnaire_btn text-white mh-parts" style="height: 40px; width: 135px;">お住まいのアンケート</button>
         </a>
       </div>
+
       <button class="header_menu_trigger ms-3 mh-parts" type="button" style="height: 60px; width: 60px;">
         <span class="batsu"></span>
       </button>
     </div>
+    
   </div>
 </header>
