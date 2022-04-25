6408213b3d9c71f590e888caf292d6c251e83341
yo-nakano <ynakano7621@gmail.com>
2022-04-25 15:10:18 +0900

    キーワード追加

--- Ebisu_Realestate_dev/resources/views/layout.blade.php
+++ Ebisu_Realestate_dev/resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="恵比寿GG,恵比寿gg,ラ・アトレ恵比寿グランガーデン,ラ・アトレ恵比寿GG">
+    <meta name="keywords" content="恵比寿GG,恵比寿gg,ラ・アトレ恵比寿グランガーデン,ラ・アトレ恵比寿GG,ラ・アトレ恵比寿マンション,恵比寿リノベーション">
     <!-- <title>【公式】ラ・アトレ恵比寿グランガーデン｜株式会社ラ・アトレ</title> -->
     <title>@yield('title')</title>
 
