12c45f5126a7c5314a746d2dfeb0211cb3418ccf
yo-nakano <ynakano7621@gmail.com>
2022-05-03 20:10:38 +0900

    bland着手

--- Ebisu_Realestate_dev/resources/views/blanding.blade.php
+++ Ebisu_Realestate_dev/resources/views/blanding.blade.php
@@ -1,6 +1,13 @@
 @extends('layout')
 
+@section('title',"Bland｜【公式】ラ・アトレ恵比寿グランガーデン")
+
 @section('content')
 
+<div class="bland-top">
+    <img  class="" src="{{ asset('/images/the bland.svg') }}">
+</div>
+
+<div style="height:50px; background-color:#000;"></div>
 
 @endsection
