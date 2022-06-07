95ebbdbaa5ce4efaf8be7664eee0e3491a2ba3bb
yo-nakano <ynakano7621@gmail.com>
2022-06-07 12:06:25 +0900

    バッチ スケジュール設定

--- Ebisu_Realestate_dev/app/Console/Kernel.php
+++ Ebisu_Realestate_dev/app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('reservation_set')->daily();
+        $schedule->command('reservation_set')->dailyAt('0:00');
     }
 
     /**
