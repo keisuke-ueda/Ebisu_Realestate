d9b6d11feb263b7d1277cc46d21d463c5aade4a1
yo-nakano <ynakano7621@gmail.com>
2022-06-07 11:32:00 +0900

    追加対応

--- Ebisu_Realestate_dev/app/Console/Commands/ReserveCommand.php
+++ Ebisu_Realestate_dev/app/Console/Commands/ReserveCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use DateTime;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ReserveCommand extends Command
 {
@@ -29,6 +30,8 @@ class ReserveCommand extends Command
      */
     public function handle()
     {
+        $days =  ['日','月', '火', '水', '木', '金', '土'];
+        
         // 本日の日付
         $today = new Datetime();
 
@@ -38,75 +41,112 @@ class ReserveCommand extends Command
 
         // 6/11以降は当日レコードを削除する
         if ($today >= $point_date) {
-            $delete_date = $today;
+            $delete_date = clone $today;
             $delete_date_w = $delete_date->format('w');
             $delete_date = $delete_date->format('Y/n/j');
 
-            DB::table('ReservationSet')
-            ->where('date', $delete_date)
-            ->delete();
+            try {
+                // Main 
+                DB::table('Main')
+                ->where('reservation_date', $delete_date)
+                ->delete();
 
-            DB::table('Reservation')
-            ->where('reservation_date', $delete_date)
-            ->delete();
 
-            echo "{$delete_date}.(.{$delete_date_w}.)のデータを削除しました。";
+                // Day
+                DB::table('Day')
+                ->where('reservation_date', $delete_date)
+                ->delete();
+
+
+                // Reservation
+                $reservation_no_array = DB::table('Time')
+                ->whereIn('reservation_time', [$delete_date.' '.'10:00〜11:30', $delete_date.' '.'13:00〜14:30', $delete_date.' '.'16:00〜17:30'])
+                ->whereNotNull('reservation_no')
+                ->pluck('reservation_no')
+                ->toArray();
+
+                DB::table('Reservation')
+                ->whereIn('reservation_no', $reservation_no_array)
+                ->delete();
+
+
+                // Time
+                DB::table('Time')
+                ->whereIn('reservation_time', [$delete_date.' '.'10:00〜11:30', $delete_date.' '.'13:00〜14:30', $delete_date.' '.'16:00〜17:30'])
+                ->delete();
+
+                Log::channel('reserve_set')->info($delete_date."(".$days[$delete_date_w].")のデータを削除しました。");
+                echo $delete_date."(".$days[$delete_date_w].")のデータを削除しました。";
+            } catch(\Exception $e) {
+                Log::channel('reserve_set')->info($e->getMessage());
+                echo $e->getMessage();
+            }
+            
         } else {
+            Log::channel('reserve_set')->info("削除データはありません");
             echo "削除データはありません";
         }
 
 
-        // 6/11以降は59日後のレコードを自動生成する
+        // 6/11以降は当日から60日後のレコードを自動生成する
 
         if ($today >= $point_date) {
-            $insert_date = $today->modify("+59 days");
+            $insert_date = clone $today;
+            $insert_date = $insert_date->modify("+60 days");
             $insert_date_w = $insert_date->format('w');
             $insert_date = $insert_date->format('Y/n/j');
 
-            // Main登録
-            DB::table('Main')
-            ->insert([
-                "reservation_date" => $insert_date,
-            ]);
-
-            // Day登録
-            $day_data = [
-                [
+            try {
+                // Main登録
+                DB::table('Main')
+                ->insert([
                     "reservation_date" => $insert_date,
-                    "reservation_time" => $insert_date.' '.'10:00〜11:30',
-                ],
-                [
-                    "reservation_date" => $insert_date,
-                    "reservation_time" => $insert_date.' '.'13:00〜14:30',
-                ],
-                [
-                    "reservation_date" => $insert_date,
-                    "reservation_time" => $insert_date.' '.'16:00〜17:30',
-                ]
-            ];
+                ]);
 
-            DB::table('Day')->insert($day_data);
+                // Day登録
+                $day_data = [
+                    [
+                        "reservation_date" => $insert_date,
+                        "reservation_time" => $insert_date.' '.'10:00〜11:30',
+                    ],
+                    [
+                        "reservation_date" => $insert_date,
+                        "reservation_time" => $insert_date.' '.'13:00〜14:30',
+                    ],
+                    [
+                        "reservation_date" => $insert_date,
+                        "reservation_time" => $insert_date.' '.'16:00〜17:30',
+                    ]
+                ];
+
+                DB::table('Day')->insert($day_data);
 
 
-            // 上限の数(3)だけTime登録
-            $time_data = [];
+                // 上限の数(3)だけTime登録
+                $time_data = [];
 
-            for ($x=0; $x<=2; $x++) {
-                array_push($time_data, ["reservation_time" => $insert_date.' '.'10:00〜11:30', "reservation_max" => 3]);
+                for ($x=0; $x<=2; $x++) {
+                    array_push($time_data, ["reservation_time" => $insert_date.' '.'10:00〜11:30', "reservation_max" => 3]);
+                }
+
+                for ($y=0; $y<=2; $y++) {
+                    array_push($time_data, ["reservation_time" => $insert_date.' '.'13:00〜14:30', "reservation_max" => 3]);
+                }
+
+                for ($z=0; $z<=2; $z++) {
+                    array_push($time_data, ["reservation_time" => $insert_date.' '.'16:00〜17:30', "reservation_max" => 3]);
+                }
+
+                DB::table('Time')->insert($time_data);
+
+                Log::channel('reserve_set')->info($insert_date."(".$days[$insert_date_w].")のデータを生成しました。");
+                echo $insert_date."(".$days[$insert_date_w].")のデータを生成しました。";
+            } catch(\Exception $e) {
+                Log::channel('reserve_set')->info($e->getMessage());
+                echo $e->getMessage();
             }
-
-            for ($y=0; $y<=2; $y++) {
-                array_push($time_data, ["reservation_time" => $insert_date.' '.'13:00〜14:30', "reservation_max" => 3]);
-            }
-
-            for ($z=0; $z<=2; $z++) {
-                array_push($time_data, ["reservation_time" => $insert_date.' '.'16:00〜17:30', "reservation_max" => 3]);
-            }
-
-            DB::table('Time')->insert($time_data);
-
-            echo "{$insert_date}.(.{$insert_date_w}.)のデータを生成しました。";
         } else {
+            Log::channel('reserve_set')->info("生成データはありません");
             echo "生成データはありません";
         }
 
