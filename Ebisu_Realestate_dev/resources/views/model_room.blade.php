6e5b3630d75a7c13cfff182df06f455d31de3776
yo-nakano <ynakano7621@gmail.com>
2022-07-03 21:22:39 +0900

    現状最新(7/3)

--- Ebisu_Realestate_dev/resources/views/model_room.blade.php
+++ Ebisu_Realestate_dev/resources/views/model_room.blade.php
@@ -34,7 +34,7 @@
     <img class="selectable-picture selectable7" data-num="7" data-title="image photo" src="{{ asset('/images/model_photo7.png') }}" alt="">
     <img class="selectable-picture selectable8" data-num="8" data-title="image photo" src="{{ asset('/images/model_photo8.png') }}" alt="">
   </div>
-  <p class="s10 text-end m-0">※掲載の写真は401号室(Cﾀｲﾌﾟ)､202号室(Dﾀｲﾌﾟ)を<br class="pc_hid">2022年6月に撮影したものです｡</p>
+  <p class="s10 model-text-end m-0">※掲載の写真は401号室(Cﾀｲﾌﾟ)､202号室(Dﾀｲﾌﾟ)を2022年6月に撮影したものです｡</p>
 </div>
 
 <div class="model-area2 bg-black ft2 w-100">
@@ -48,19 +48,19 @@
         </div>
         <div class="pictures" style="">
           <div class="">
-            <img class="d-block w-100" src="{{ asset('/images/IHクッキングヒーター.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/IH heater.png') }}" alt="">
             <p class="s18 m-0 gold-2">IH クッキングヒーター</p>
           </div>
           <div class="kitchen-logo">
-            <img class="d-block mx-auto" src="{{ asset('/images/kitchen.svg') }}" alt="" style="aspect-ratio: unset; margin-bottom:10px; width:90%;">
-            <p class="s10 m-0" style="width: 115%;">※掲載の写真は401号室(Cﾀｲﾌﾟ)を<br class="pc_hid">2022年6月に撮影したものです｡</p>
+            <img class="d-block" src="{{ asset('/images/kitchen.svg') }}" alt="" style="aspect-ratio: unset; margin-bottom:10px;">
+            <p class="s10 m-0" style="">※掲載の写真は401号室(Cﾀｲﾌﾟ)を2022年6月に撮影したものです｡</p>
           </div>
           <div class="">
-            <img class="d-block w-100" src="{{ asset('/images/ハンズフリー水栓.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/hands-free.png') }}" alt="">
             <p class="s18 m-0 gold-2">ハンズフリー水栓</p>
           </div>
           <div class="">
-            <img class="d-block w-100" src="{{ asset('/images/多機能オーブンレンジ.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/oven.png') }}" alt="">
             <p class="s18 m-0 gold-2">多機能オーブンレンジ</p>
           </div>
           <div class="">
@@ -79,15 +79,15 @@
           <p class="s18 m-0 gold-2">静音シンク</p>
         </div>
         <div class="">
-          <img class="d-block w-100" src="{{ asset('/images/スリム型レンジフード.png') }}" alt="">
+          <img class="d-block w-100" src="{{ asset('/images/slim-food.png') }}" alt="">
           <p class="s18 m-0 gold-2">スリム型レンジフード</p>
         </div>
         <div class="">
-          <img class="d-block w-100" src="{{ asset('/images/ビルトイン食器洗い乾燥機.png') }}" alt="">
+          <img class="d-block w-100" src="{{ asset('/images/option.png') }}" alt="">
           <p class="s18 m-0 gold-2">ビルトイン食器洗い乾燥機<br>(オプション)</p>
         </div>
         <div class="">
-          <img class="d-block w-100" src="{{ asset('/images/スライド式キッチン収納.png') }}" alt="">
+          <img class="d-block w-100" src="{{ asset('/images/slide-kitchen.png') }}" alt="">
           <p class="s18 m-0 gold-2">スライド式キッチン収納</p>
         </div>
       </div>
@@ -98,21 +98,21 @@
       <div class="w-100 bathroom-content" style="">
         <div class="bathroom-pictures1">
           <div class="bathroom202">
-            <img class="d-block w-100" src="{{ asset('/images/バスルーム202.png') }}" alt="">
-            <p class="s14 m-0 text-end">バスルーム202号室(Dﾀｲﾌﾟ)</p>
+            <img class="d-block w-100" src="{{ asset('/images/bathroom202.png') }}" alt="">
+            <p class="s14 m-0 model-text-end">バスルーム202号室(Dﾀｲﾌﾟ)</p>
           </div>
           <div class="small_picture">
-            <img class="d-block w-100" src="{{ asset('/images/バスリモコン.jpg') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/bath-remote.jpg') }}" alt="">
             <p class="s18 m-0 gold-2">バスリモコン</p>
           </div>
           <div class="small_picture">
-            <img class="d-block w-100" src="{{ asset('/images/バスルームタイル.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/bathroom-tile.png') }}" alt="">
             <p class="s18 m-0 gold-2">バスルームタイル</p>
           </div>
         </div>
         <div class="bathroom-pictures2">
           <div class="small_picture">
-            <img class="d-block w-100" src="{{ asset('/images/バスタブ.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/bath-tab.png') }}" alt="">
             <p class="s18 m-0 gold-2">バスタブ</p>
           </div>
           <div class="bathroom-logo">
@@ -120,7 +120,7 @@
             <p class="s10 m-0">※掲載の写真は401号室(Cﾀｲﾌﾟ)､202号室(Dﾀｲﾌﾟ)を<br class="sp_hid"><span class="pc_indent">&nbsp;&nbsp;&nbsp;</span>2022年6月に撮影したものです｡</p>
           </div>
           <div class="small_picture">
-            <img class="d-block w-100" src="{{ asset('/images/スライド式シャワーヘッド.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/shower-head.png') }}" alt="" style="object-position:50% 40%;">
             <p class="s18 m-0 gold-2">スライド式シャワーヘッド</p>
           </div>
           <div class="small_picture">
@@ -128,16 +128,16 @@
             <p class="s18 m-0 gold-2">浴室暖房乾燥機</p>
           </div>
           <div class="small_picture pc_hid">
-            <img class="d-block w-100" src="{{ asset('/images/バスリモコン.jpg') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/bath-remote.jpg') }}" alt="">
             <p class="s18 m-0 gold-2">バスリモコン</p>
           </div>
           <div class="small_picture pc_hid">
-            <img class="d-block w-100" src="{{ asset('/images/バスルームタイル.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/bathroom-tile.png') }}" alt="">
             <p class="s18 m-0 gold-2">バスルームタイル</p>
           </div>
           <div class="bathroom401">
-            <img class="d-block w-100" src="{{ asset('/images/バスルーム401.jpg') }}" alt="">
-            <p class="s14 m-0 text-end">バスルーム401号室(Cﾀｲﾌﾟ)</p>
+            <img class="d-block w-100" src="{{ asset('/images/bathroom401.jpg') }}" alt="">
+            <p class="s14 m-0 model-text-end">バスルーム401号室(Cﾀｲﾌﾟ)</p>
           </div>
         </div>
       </div>
@@ -149,14 +149,14 @@
         <div class="lavatory-pictures1">
           <div class="lavatory202">
             <img class="d-block w-100" src="{{ asset('/images/洗面室202.png') }}" alt="">
-            <p class="s14 m-0 text-end">洗面室202号室(Dﾀｲﾌﾟ)</p>
+            <p class="s14 m-0 model-text-end">洗面室202号室(Dﾀｲﾌﾟ)</p>
           </div>
           <div class="small_picture">
             <img class="d-block w-100" src="{{ asset('/images/洗面人造大理石カウンター天板.png') }}" alt="">
             <p class="s18 m-0 gold-2">洗面人造大理石カウンター天板</p>
           </div>
           <div class="small_picture">
-            <img class="d-block w-100" src="{{ asset('/images/洗面ボウル.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/bowl.png') }}" alt="">
             <p class="s18 m-0 gold-2">洗面ボウル</p>
           </div>
         </div>
@@ -166,9 +166,9 @@
             <p class="s18 m-0 gold-2">タオルかけ</p>
           </div>
           <div class="small_picture1 lavatory-logo">
-            <div>
+            <div class="lavatory-logo-top">
               <img class="d-block w-100" src="{{ asset('/images/lavatory.svg') }}" alt="" style="aspect-ratio:unset; margin-bottom:10px;">
-              <p class="s10 m-0">※掲載の写真は401号室(Cﾀｲﾌﾟ)､202号室(Dﾀｲﾌﾟ)を<br><span class="pc_indent">&nbsp;&nbsp;&nbsp;</span>2022年6月に撮影したものです｡</p>
+              <p class="s10 m-0">※掲載の写真は401号室(Cﾀｲﾌﾟ)､202号室(Dﾀｲﾌﾟ)を2022年6月に撮影したものです｡</p>
             </div>
             <div>
               <img class="d-block w-100 picture1-2" src="{{ asset('/images/トイレリモコン.png') }}" alt="">
@@ -180,7 +180,7 @@
             <p class="s18 m-0 gold-2">三面鏡収納</p>
           </div>
           <div class="small_picture2">
-            <img class="d-block w-100" src="{{ asset('/images/シングルレバー水栓.png') }}" alt="">
+            <img class="d-block w-100" src="{{ asset('/images/single.png') }}" alt="">
             <p class="s18 m-0 gold-2">シングルレバー水栓</p>
           </div>
           <div class="small_picture2">
@@ -189,14 +189,14 @@
           </div>
           <div class="lavatory401">
             <img class="d-block w-100" src="{{ asset('/images/洗面室401.png') }}" alt="">
-            <p class="s14 m-0 text-end">洗面室401号室(Cﾀｲﾌﾟ)</p>
+            <p class="s14 m-0 model-text-end">洗面室(2)401号室(Cﾀｲﾌﾟ)</p>
           </div>
           <div class="small_picture1 pc_hid">
             <img class="d-block w-100" src="{{ asset('/images/洗面人造大理石カウンター天板.png') }}" alt="" style="margin-bottom:5px;">
             <p class="s18 m-0 gold-2">洗面人造大理石カウンター天板</p>
           </div>
           <div class="small_picture1 pc_hid">
-            <img class="d-block w-100" src="{{ asset('/images/洗面ボウル.png') }}" alt="" style="margin-bottom:5px;">
+            <img class="d-block w-100" src="{{ asset('/images/bowl.png') }}" alt="" style="margin-bottom:5px;">
             <p class="s18 m-0 gold-2">洗面ボウル</p>
           </div>
         </div>
@@ -211,14 +211,14 @@
           <p class="s14 m-0">玄関</p>
         </div>
         <!-- PC表示 -->
-        <div class="right sp_hid" style="width:calc(50% - 10px);">
-          <div class="w-100" style="margin-bottom:70px;">
-            <img class="d-block mx-auto" src="{{ asset('/images/storage.svg') }}" alt="" style="width:50%; margin-bottom:10px; padding-top:80px;">
+        <div class="right sp_hid" style="width:calc(50% - 10px); height:100%;">
+          <div class="w-100" style="margin-bottom:7.5vw;">
+            <img class="d-block mx-auto" src="{{ asset('/images/storage.svg') }}" alt="" style="width:50%; margin-bottom:10px;">
             <p class="s10 m-0 text-center">※掲載の写真は401号室(Cﾀｲﾌﾟ)を2022年6月に撮影したものです｡</p>
           </div>
-          <div class="d-flex justify-content-between w-100">
+          <div class="d-flex justify-content-between align-items-end w-100">
             <div class="" style="width:calc(32.6% - 10px);">
-              <div class="w-100" style="margin-bottom:20px;">
+              <div class="w-100" style="margin-bottom:1.8vw;">
                 <img class="d-block w-100" src="{{ asset('/images/寝室WIC.jpg') }}" alt="" style="aspect-ratio:17/26; object-fit:cover; margin-bottom:10px;">
                 <p class="s18 m-0 gold-2">寝室WIC</p>
               </div>
@@ -228,7 +228,7 @@
               </div>
             </div>
             <div class="" style="width:calc(67.4% - 10px);" >
-              <img class="d-block w-100" src="{{ asset('/images/シューズクローゼット.jpg') }}" alt="" style="aspect-ratio:5/8; object-fit:cover; margin-bottom:10px;">
+              <img class="d-block w-100" src="{{ asset('/images/shoos-closet.jpg') }}" alt="" style="aspect-ratio:5/8; object-fit:cover; margin-bottom:10px;">
               <p class="s18 m-0 gold-2">シューズクローゼット</p>
             </div>
           </div>
@@ -239,7 +239,7 @@
           <div class="w-100 d-flex justify-content-between align-items-end">
             <div class="" style="width:calc(50% - 5px);">
               <div class="w-100" style="margin-bottom:20px;" >
-                <img class="d-block w-100" src="{{ asset('/images/シューズクローゼット.jpg') }}" alt="" style="aspect-ratio:5/6; object-fit:cover; margin-bottom:5px;">
+                <img class="d-block w-100" src="{{ asset('/images/shoos-closet.jpg') }}" alt="" style="aspect-ratio:5/6; object-fit:cover; margin-bottom:5px;">
                 <p class="s18 m-0 gold-2">シューズクローゼット</p>
               </div>
               <div class="w-100">
@@ -248,7 +248,7 @@
               </div>
             </div>
             <div class="" style="width:calc(50% - 5px);">
-              <div class="w-100" style="margin-bottom:50px;">
+              <div class="w-100" style="margin-bottom: 14vw;">
                 <img class="d-block mx-auto" src="{{ asset('/images/storage.svg') }}" alt="" style="width:90%; margin-bottom:10px; padding-top:0px;">
                 <p class="s10 m-0 text-center">※掲載の写真は401号室(Cﾀｲﾌﾟ)を2022年6月に撮影したものです｡</p>
               </div>
@@ -270,18 +270,26 @@
           <p class="s18 m-0 gold-2">アクセントタイル</p>
         </div>
         <div class="other-picture2 sp_hid">
-          <img class="d-block w-100" src="{{ asset('/images/ドアストッパー.jpg') }}" alt="">
+          <img class="d-block w-100" src="{{ asset('/images/door-stopper.jpg') }}" alt="">
           <p class="s18 m-0 gold-2">ドアストッパー</p>
         </div>
         <div class="other-picture3 sp_hid">
           <img class="d-block w-100" src="{{ asset('/images/人感センサー.jpg') }}" alt="">
           <p class="s18 m-0 gold-2">人感センサー</p>
         </div>
-        <div class="other-logo">
+        <div class="other-logo sp_hid">
           <img class="d-block other-logo-img" src="{{ asset('/images/other.svg') }}" alt="" style="">
           <p class="s10" style="margin-bottom:15px;">※掲載の写真は401号室(Cﾀｲﾌﾟ)､202号室(Dﾀｲﾌﾟ)を<br class="sp_hid"><span class="pc_indent">&nbsp;&nbsp;&nbsp;</span>2022年6月に撮影したものです｡</p>
-          <img class="w-100 pc_hid" src="{{ asset('/images/ドアストッパー.jpg') }}" alt="" style="aspect-ratio: 260/85; object-fit: cover; margin-bottom:5px;">
-          <p class="s18 m-0 gold-2 pc_hid">ドアストッパー</p>
+        </div>
+        <div class="other-logo-sp pc_hid">
+          <div style="margin-bottom:calc(8vw - 10px);">
+            <img class="d-block other-logo-img" src="{{ asset('/images/other.svg') }}" alt="" style="margin-bottom:10px;">
+            <p class="s10 m-0" style="">※掲載の写真は401号室(Cﾀｲﾌﾟ)､202号室(Dﾀｲﾌﾟ)を<br class="sp_hid"><span class="pc_indent">&nbsp;&nbsp;&nbsp;</span>2022年6月に撮影したものです｡</p>
+          </div>
+          <div>
+            <img class="w-100" src="{{ asset('/images/door-stopper.jpg') }}" alt="" style="aspect-ratio: 260/85; object-fit: cover; margin:0 0 5px;">
+            <p class="s18 m-0 gold-2">ドアストッパー</p>
+          </div>
         </div>
         <div class="other-picture3 pc_hid">
           <img class="d-block w-100" src="{{ asset('/images/人感センサー.jpg') }}" alt="">
@@ -292,7 +300,7 @@
           <p class="s18 m-0 gold-2">調光機能付きスイッチ</p>
         </div>
         <div class="other-picture5">
-          <img class="d-block w-100" src="{{ asset('/images/ワイドスイッチ.jpg') }}" alt="">
+          <img class="d-block w-100" src="{{ asset('/images/wide-switch.jpg') }}" alt="">
           <p class="s18 m-0 gold-2">ワイドスイッチ</p>
         </div>
         <div class="other-picture6">
