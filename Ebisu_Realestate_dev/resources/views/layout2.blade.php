e0d69bbb2175ae3c92cd309c62b14b4ef16fbbb0
yo-nakano <ynakano7621@gmail.com>
2022-07-13 16:56:17 +0900

    リスティングタグ設置

--- Ebisu_Realestate_dev/resources/views/layout2.blade.php
+++ Ebisu_Realestate_dev/resources/views/layout2.blade.php
@@ -66,5 +66,19 @@
     <!-- フッター -->
     @include('footer')
     </div>
+
+    <!-- リスティング用タグ -->
+    <script type="text/javascript">
+        (function () {
+            var tagjs = document.createElement("script");
+            var s = document.getElementsByTagName("script")[0];
+            tagjs.async = true;
+            tagjs.src = "//s.yjtag.jp/tag.js#site=k5EVl6m";
+            s.parentNode.insertBefore(tagjs, s);
+        }());
+    </script>
+    <noscript>
+        <iframe src="//b.yjtag.jp/iframe?c=k5EVl6m" width="1" height="1" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </noscript>
 </body>
 </html>
